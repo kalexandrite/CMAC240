b8f17a471dbdaa332dd2eff20a3f916fe4699ed5
katealexandrite <katherine.guillen@duke.edu>
2022-11-14 08:11:53 -0500

    update images

---
+++ script2.js
@@ -0,0 +1,8 @@
+function revealMessage() {
+    document.getElementById('hiddenMessage').style.display = 'block';
+
+}
+function revealMessage2() {
+    document.getElementById('hiddenMessage2').style.display = 'block';
+
+}
